3b69ee1b6c90b9cd11e56b2010b865b7fe31769a
krish-19 <90400710+krish-19@users.noreply.github.com>
2022-04-09 18:44:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,54 @@
-const Engine = Matter.Engine;
-const Render = Matter.Render;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-const Body = Matter.Body;
-const Composites = Matter.Composites;
-const Composite = Matter.Composite;
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+var score =0;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+var player1score =0;
+var player2score =0;
 
-let engine;
-let world;
-var ball;
-var blower;
-var blowerMouth;
-var button;
+var obstacleGroup,obstacle,obstacleImage;
 
+function preload(){
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruit5_img = loadImage("images/pineapple2.png");
+  fruitGroup = new Group();
+
+  obstacleImage = loadImage("images/mushroom.png")
+}
 function setup() {
-  var canvas = createCanvas(500, 500);
-
-  engine = Engine.create();
-  world = engine.world;
-
-  ball = new Ball(width / 2 + 80, height / 2 - 80, 80, 80);
-  blower = new Blower(width / 2 - 50, height / 2 + 50, 150, 20);
-  blowerMouth = new BlowerMouth(width / 2 + 70, height / 2 + 20, 100, 90);
-  button = createImg('click.png');
-button.position(20,30);
-button.size(50,50);
-
-
-// buttonmouseClicked(blow);
-
-// button.mouseClicked(blow);
-
-// button.Clicked(blow);
-
-// button.mouse(blow);
-
-
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
 }
 
 function draw() {
-  background(59);
-  Engine.update(engine);
-
-  blower.show();
-  ball.show();
-  blowerMouth.show();
-}
-
-function blow() {
+  background(back_img);
   
-  // Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: -0.05 });
-  
-  // Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0.05, y: 0.05 });
-  
-  //Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: 0.05 });
-  
-  // Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: -0.05, y: 0.05 });
-
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+    
+     game.end();
+   }
 }
